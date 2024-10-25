d83601caec72503f603ca34d531f2c4756dd1245
E. Anne Gunn <egunn@sheridan.edu>
2024-10-25 11:42:28 -0600

    Update a second icon

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.forever(function () {
     for (let index = 0; index < 4; index++) {
         basic.showIcon(IconNames.Skull)
         basic.pause(100)
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Ghost)
         basic.pause(100)
     }
 })
