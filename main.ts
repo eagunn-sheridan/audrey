f67be62d50d9e0911ad7ba03f24a442a67421c4b
E. Anne Gunn <egunn@sheridan.edu>
2024-10-25 11:39:16 -0600

    Added a second icon, a pause, and a loop

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 basic.forever(function () {
-    basic.showIcon(IconNames.Skull)
-    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Skull)
+        basic.pause(100)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+    }
 })
